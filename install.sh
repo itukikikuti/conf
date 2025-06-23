79f29f5c253e7193b130933a6485226f6af0f7fc
itukikikuti <xxvraqwa@gmail.com>
2025-06-24 08:41:04 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/itukikikuti/server-auto-install/refs/heads/main/create-vm-template.sh
-chmod +x create-vm-template.sh
-./create-vm-template.sh
-rm create-vm-template.sh
